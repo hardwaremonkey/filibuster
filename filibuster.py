8f57b62777dcc03b0990aa45f10406113fe3b92e
hardwaremonkey <matt.oppenheim@gmail.com>
2021-06-04 11:52:02 +0100

    Extracting search terms from search_terms.txt file.

--- filibuster.py
+++ filibuster.py
@@ -6,14 +6,16 @@ import PyPDF2
 DIR = os.path.dirname(os.path.realpath(__file__))
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logging.info('started filibuster')
-search_terms = ['the', 'relevant']
+SEARCH_TERMS_FILENAME = 'search_terms.txt'
 
 
-def count_words(text, search_term):
+def count_search_terms(text, search_terms):
     ''' Count how many times search_term occurs in text. '''
     text = text.lower().split()
-    search_term_count = text.count(search_term.lower())
-    return search_term_count
+    for search_term in search_terms:
+        search_term = search_term.strip()
+        search_term_count = text.count(search_term.lower())
+        logging.info('{} occurs {} times'.format(search_term, search_term_count))
 
 
 def docx_to_text(filepath):
@@ -24,10 +26,26 @@ def docx_to_text(filepath):
     return text
 
 
+def exit_code(message):
+    ''' exits '''
+    print(message)
+    print("exiting")
+    raise SystemExit
+
+
 def get_filepath(dir, filename):
     ''' Create filepath. '''
     return os.path.join(DIR, filename)
 
+
+def get_search_terms():
+    ''' Extract list of search terms from file.'''
+    search_terms = []
+    with open(os.path.join(DIR, SEARCH_TERMS_FILENAME), 'r') as search_terms_file:
+        for search_term in search_terms_file:
+            search_terms.append(search_term)
+    return search_terms
+        
 
 def pdf_to_text(filepath):
     ''' Extract text from pdf files. '''
@@ -42,13 +60,11 @@ def pdf_to_text(filepath):
     return all_text
 
 
-def count_search_terms(text, search_terms):
-    ''' Count the occurrences of search terms in text. '''
-    for search_term in search_terms:
-        count_word = count_words(text, search_term)
-        logging.info('\t{} occurs {} times'.format(search_term, count_word))
-
 def main():
+    search_terms_filepath = os.path.join(DIR, SEARCH_TERMS_FILENAME)
+    if not os.path.exists(search_terms_filepath):
+        exit_code('search terms file does not exist: {}'.format(search_terms_filepath))
+    search_terms = get_search_terms()
     logging.info('searching for Word and pdf files in: {}'.format(DIR))
     for filename in os.listdir(DIR):
         logging.info('found {}'.format(filename))
